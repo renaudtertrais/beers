67e1dcbacdcc0d621c850f7ca07dd348edcc7803
Renaud Tertrais <renaud.tertrais@gmail.com>
2019-06-12 19:15:10 +0200

    Updates

---
+++ precache-manifest.9f9b7574770b7671e5afc3eee04caa2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6084108e223e99f946b75a4736f1186a",
+    "url": "/beers/index.html"
+  },
+  {
+    "revision": "aa7159305a7eff74261b",
+    "url": "/beers/static/css/2.ab7e5612.chunk.css"
+  },
+  {
+    "revision": "4c34de74d0f2fff64840",
+    "url": "/beers/static/css/main.3d143f69.chunk.css"
+  },
+  {
+    "revision": "aa7159305a7eff74261b",
+    "url": "/beers/static/js/2.9a6a02b9.chunk.js"
+  },
+  {
+    "revision": "4c34de74d0f2fff64840",
+    "url": "/beers/static/js/main.f312c65c.chunk.js"
+  },
+  {
+    "revision": "e4ad694ab96731d20d82",
+    "url": "/beers/static/js/runtime~main.fb6b770c.js"
+  }
+]);
